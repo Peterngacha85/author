253b15dff90879daf87e6470bf0f75e7f89fee7d
Annkahoro <annkahoro@gmail.com>
2026-03-13 14:14:49 +0300

    Add 'developed by Fastweb Technologies' link to footer

--- fronted/src/pages/public/LandingPage.jsx
+++ fronted/src/pages/public/LandingPage.jsx
@@ -293,7 +293,7 @@ export default function LandingPage() {
             </div>
           </div>
         </div>
-          © 2026 Joe Books. All rights reserved.
+          © 2026 Joe Books. All rights reserved. developed by <a href="https://fastweb.co.ke" style={{ color: colors.accentRed, textDecoration: 'none' }} target="_blank" rel="noopener noreferrer">Fastweb Technologies</a>
       </footer>
     </div>
   );
